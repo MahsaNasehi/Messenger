5d8474859bd3bb79713e9160e08c4440ccb14947
Mahsa <manmahna26@gmail.com>
2024-06-10 19:38:51 +0330

    insertion methods(inserting new records into tables)

--- Massenger/CreationInsertionQueryFile.sql
+++ Massenger/CreationInsertionQueryFile.sql
@@ -12,8 +12,8 @@ CREATE TABLE  IF NOT EXISTS contacts(
     user_id INT,
     user_s_contact_id INT,
     PRIMARY KEY (user_id, user_s_contact_id),
-    FOREIGN KEY(user_id) references user(user_id),
-    FOREIGN KEY(user_s_contact_id) references user(user_id)
+    FOREIGN KEY(user_id) references user(user_id)ON DELETE CASCADE ON UPDATE CASCADE ,
+    FOREIGN KEY(user_s_contact_id) references user(user_id)ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS  group_chat(
@@ -25,16 +25,16 @@ CREATE TABLE IF NOT EXISTS pv_chat(
     chat_id  INT PRIMARY KEY AUTO_INCREMENT,
     user1_id INT NOT NULL,
     user2_id INT NOT NULL,
-    FOREIGN KEY (user1_id) REFERENCES user(user_id),
-    FOREIGN KEY (user1_id) REFERENCES user(user_id)
+    FOREIGN KEY (user1_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+    FOREIGN KEY (user1_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS group_membership(
     chat_id INT,
     member_id INT,
     PRIMARY KEY (chat_id, member_id),
-    FOREIGN KEY (chat_id) REFERENCES group_chat(chat_id),
-    FOREIGN KEY  (member_id) REFERENCES user(user_id)
+    FOREIGN KEY (chat_id) REFERENCES group_chat(chat_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+    FOREIGN KEY  (member_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS  msg(
@@ -42,14 +42,24 @@ CREATE TABLE IF NOT EXISTS  msg(
     chat_id INT DEFAULT NULL,
     pv_id INT DEFAULT NULL,
     sender_id INT NOT NULL ,
-    content VARCHAR(50) NOT NULL,
-    CONSTRAINT enforce_one_not_null CHECK (msg.chat_id IS NOT NULL XOR msg.pv_id IS NOT NULL),
-    FOREIGN KEY (chat_id) REFERENCES group_chat(chat_id),
-    FOREIGN KEY (pv_id) REFERENCES pv_chat(chat_id),
-    FOREIGN KEY (sender_id) REFERENCES user(user_id)
+    content VARCHAR(100) NOT NULL,
+#     CONSTRAINT enforce_one_not_null CHECK (chat_id IS NULL XOR pv_id IS NULL),
+    FOREIGN KEY (chat_id) REFERENCES group_chat(chat_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+    FOREIGN KEY (pv_id) REFERENCES pv_chat(chat_id) ON DELETE CASCADE ON UPDATE CASCADE ,
+    FOREIGN KEY (sender_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
+# # trigger for constraint in msg
+#     CREATE TRIGGER enforce_one_not_null_msg BEFORE INSERT ON msg
+#     FOR EACH ROW
+#     BEGIN
+#         IF NEW.chat_id IS NULL AND NEW.pv_id IS NULL THEN
+#             SIGNAL SQLSTATE '45000'  'Either chat_id or pv_id must be not null';
+#         ELSEIF NEW.chat_id IS NOT NULL AND NEW.pv_id IS NOT NULL THEN
+#                 SIGNAL SQLSTATE '45000' SET MESSAGE 'Only one of chat_id or pv_id can be not null';
+#         END IF;
+#     END
+# #     // DELIMITER ;
 ## insert queries
 INSERT INTO user(user_name, first_name, last_name, phone_number) VALUES
     ('u1', 'mahsa', 'nasehi', '09111111111'),
@@ -59,7 +69,8 @@ INSERT INTO user(user_name, first_name, last_name, phone_number) VALUES
     ('u5', 'mona', 'eradi', '09111111115'),
     ('u6', 'zahra', 'saha', '09111111116');
 
-
+INSERT INTO user(user_name, first_name, last_name, phone_number) VALUES
+    ('u7', 'mahan', 'nasehi', '09111111117');
 INSERT INTO contacts(user_id, user_s_contact_id) VALUES
     ('1', '2'),
     ('1', '3'),
